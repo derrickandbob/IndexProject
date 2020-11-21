f0c118f3d623815d8e93d7f2183d141f7021d6b8
Sam Robert <robertksam2000@gmail.com>
2020-11-21 18:31:20 +0000

    JS Done

--- App.js
+++ App.js
@@ -1,60 +1,131 @@
-// Selectors
-const offButton = document.querySelector(".powerOff");
-const onButton = document.querySelector(".powerOn");
-const screen = document.querySelector(".screen");
-const allNumbers = document.querySelectorAll(".numb")
-const allOperators = document.querySelectorAll(".operator")
-const clearScreen = document.querySelector(".clearBtn")
-const solve = document.querySelector(".answer")
+// Selects elements in html
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let offButton = document.querySelector("#off")
+let onButton = document.querySelector("#on")
+let profile = document.querySelector(".profile")
+let aboutUs = document.querySelector(".aboutUs")
+let result = document.querySelector("#result")
 
-
-// // Turn off calculator
-// offButton.addEventListener("click",()=>{
-//     if(offButton.classList.contains(".powerOff")){
-//         console.log("Yeah")
-//         disableCalculator()
-//     }
-// })
-
-// // Disable all buttons onload
-// document.addEventListener("DOMContentload",()=>{
-//     allNumbers.disabled == true;
-//     allOperators.disabled == true;
-// })
-
-
-// //Disable Calculator
-// function disableCalculator(){
-//     allNumbers.disabled = true
-//     allOperators.disabled = true
-//     screen.innerHTML = "Power off"
-// }
-// Get all the numbers onclicked
-allNumbers.forEach(userInput=>{
-    userInput.addEventListener("click",()=>{
-        let displayUserValue = userInput.value; 
-        screen.textContent += displayUserValue;
-        screen.classList.add("styleUserInput")
+// Disables all numbers when clicked
+offButton.addEventListener("click",()=>{
+    numbers.forEach(eachNumber=>{
+        eachNumber.disabled = true;
     })
-
-})
-
-
-
-
-// Get all the operators onclicked
-allOperators.forEach(operator=>{
-    operator.addEventListener("click",()=>{
-        screen.textContent += operator.value;
-        screen.classList.add("styleUserInput")
+    operators.forEach(eachOperator=>{
+        eachOperator.disabled = true
     })
 })
 
+// Enables all buttons when clicked
+onButton.addEventListener("click",()=>{
+    numbers.forEach(forEachNumber=>{
+        forEachNumber.disabled = false;
 
-// Clear Screen
-clearScreen.addEventListener("click",()=>{
-    screen.innerHTML = "";
+        numbers.forEach(getNumber=>{
+            aboutUs.style.display = "none"
+            result.style.display = "block"
+            getNumber.style.display = "block"
+        })
+        operators.forEach(getOperator=>{
+            getOperator.style.display = "block"
+        })
+
+    })
+    operators.forEach(forEachOperator=>{
+        forEachOperator.disabled = false
+    })
 })
 
+// Functions
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+    return document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+    if(num==""){
+        document.getElementById("output-value").innerText = num;
+    }
+    else{
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+function getFormattedNumber(num){
+    if(num =="-"){
+        return ""
+    }
+    let n = Number(num);
+    let value = n.toLocaleString("en")
+    return value
+}
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''))
+}
+let operator = document.getElementsByClassName("operator")
+for(let i=0; i<operator.length; i++){
+    operator[i].addEventListener("click",function(){
+        if(this.id == "clear"){
+            printHistory("")
+            printOutput("")
+        }
+        if(this.id == "backspace"){
+            let output = reverseNumberFormat(getOutput()).toString();
+            if(output){
+                output = output.substr(0,output.length-1)
+                printOutput(output)
+            }
+        }
+        else{
+            let output = getOutput()
+            let history = getHistory()
+            if(output==""&&history!=""){
+                if(isNaN(history[history.length-1])){
+                    history = history.substr(0,history.length-1)
+                }
+            }
+            if(output!="" || history!=""){
+                output = output==""? output:reverseNumberFormat(output)
+                history = history + output
+                if(this.id=="="){
+                    let result = eval(history)
+                    printOutput(result)
+                    printHistory("")
+                }
+                else{
+                    history = history + this.id
+                    printHistory(history)
+                    printOutput("")
+                }
+            }
+        }
+    })
+}
 
+// Show About Us Page
+profile.addEventListener("click",function(){
+    numbers.forEach(getNumber=>{
+        getNumber.style.display = "none"
+        result.style.display = "none"
+    })
+    operators.forEach(getOperator=>{
+        getOperator.style.display = "none"
+        aboutUs.style.display = "block"
+    })
+})
 
+// Prints output on screen
+let number = document.getElementsByClassName("number")
+for(let i=0; i<number.length; i++){
+    number[i].addEventListener("click",function(){
+        let output = reverseNumberFormat(getOutput())
+        if(output!=NaN){
+            output = output + this.id
+            printOutput(output)
+        }
+    })
+}
